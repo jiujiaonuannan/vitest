bdd6177105802ed80a7837b1560450f354b848be
Madman804 <alvin.wang1202@gmail.com>
2022-06-16 22:32:43 +0200

    chore: add parse fn

--- src/Parser.ts
+++ src/Parser.ts
@@ -100,4 +100,133 @@ export class Parser {
     this._currentIndex++;
     return currentToken;
   }
+
+  private _checkCurrentTokenType(type: TokenType | TokenType[]): boolean {
+    if (this._isEnd()) {
+      return false;
+    }
+    const currentToken = this._tokens[this._currentIndex];
+    if (Array.isArray(type)) {
+      return type.includes(currentToken.type);
+    } else {
+      return currentToken.type === type;
+    }
+  }
+
+  private _getCurrentToken(): Token {
+    return this._tokens[this._currentIndex];
+  }
+  
+  private _getPreviousToken(): Token {
+    return this._tokens[this._currentIndex - 1];
+  }
+
+  private _parseStatement(): Statement {
+    // TokenType 来自 Tokenizer 的实现中
+    if (this._checkCurrentTokenType(TokenType.Let)) {
+      return this._parseVariableDeclaration();
+    }
+    throw new Error("Unexpected token");
+  }
+  // 1. 解析变量名
+  private _parseIdentifier(): Identifier {
+    const token = this._getCurrentToken();
+    const identifier: Identifier = {
+      type: NodeType.Identifier,
+      name: token.value!,
+      start: token.start,
+      end: token.end,
+    };
+    this._goNext(TokenType.Identifier);
+    return identifier;
+  }
+
+  // 2. 解析函数表达式
+  private _parseFunctionExpression(): FunctionExpression {
+    const { start } = this._getCurrentToken();
+    this._goNext(TokenType.Function);
+    let id = null;
+    if (this._checkCurrentTokenType(TokenType.Identifier)) {
+      id = this._parseIdentifier();
+    }
+    const node: FunctionExpression = {
+      type: NodeType.FunctionExpression,
+      id,
+      params: [],
+      body: {
+        type: NodeType.BlockStatement,
+        body: [],
+        start,
+        end: Infinity,
+      },
+      start,
+      end: 0,
+    };
+    return node;
+  }
+
+  // 用于解析函数参数
+  private _parseParams(): Identifier[] | Expression[] {
+    // 消费 "("
+    this._goNext(TokenType.LeftParen);
+    const params = [];
+    // 逐个解析括号中的参数
+    while (!this._checkCurrentTokenType(TokenType.RightParen)) {
+      let param = this._parseIdentifier();
+      params.push(param);
+    }
+    // 消费 ")"
+    this._goNext(TokenType.RightParen);
+    return params;
+  }
+
+  // 用于解析函数体
+  private _parseBlockStatement(): BlockStatement {
+    const { start } = this._getCurrentToken();
+    const blockStatement: BlockStatement = {
+      type: NodeType.BlockStatement,
+      body: [],
+      start,
+      end: Infinity,
+    };
+    // 消费 "{"
+    this._goNext(TokenType.LeftCurly);
+    while (!this._checkCurrentTokenType(TokenType.RightCurly)) {
+      // 递归调用 _parseStatement 解析函数体中的语句(Statement)
+      const node = this._parseStatement();
+      blockStatement.body.push(node);
+    }
+    blockStatement.end = this._getCurrentToken().end;
+    // 消费 "}"
+    this._goNext(TokenType.RightCurly);
+    return blockStatement;
+  }
+
+  private _parseVariableDeclaration(): VariableDeclaration {
+    // 获取语句开始位置
+    const { start } = this._getCurrentToken();
+    // 拿到 let
+    const kind = this._getCurrentToken().value;
+    this._goNext(TokenType.Let);
+    // 解析变量名 foo
+    const id = this._parseIdentifier();
+    // 解析函数表达式
+    const init = this._parseFunctionExpression();
+    const declarator: VariableDeclarator = {
+      type: NodeType.VariableDeclarator,
+      id,
+      init,
+      start: id.start,
+      end: init ? init.end : id.end,
+    };
+    // 构造 Declaration 节点
+    const node: VariableDeclaration = {
+      type: NodeType.VariableDeclaration,
+      kind: kind as VariableKind,
+      declarations: [declarator],
+      start,
+      end: this._getPreviousToken().end,
+    };
+    return node;
+  }
 }
